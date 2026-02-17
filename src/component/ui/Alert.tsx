8356d215b4283a984437470402c4a321caf09010
HOANG\hoang <hoangtran234176@gmail.com>
2026-02-18 00:34:46 +0700

    feat(UI): add component UI alert with basic style

---
+++ src/component/ui/Alert.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import "./Alert.css"
+
+
+interface AlertProps {
+    show: boolean,
+    title?: "" | "THÔNG BÁO" | "CẢNH BÁO" | "LỖI" ,
+    type: 'success' | 'error' | 'warning' | 'info',
+    message: string,
+    action?: React.ReactNode,
+}
+
+
+const Alert = ({
+    show,
+    title,
+    type,
+    message,
+    action
+} : AlertProps) => {
+    if(!show){
+        return null;
+    } else {
+        return (
+            <>
+                <div className="alert-overlay"></div>
+                <div className="alert-group">
+                    <div className={`title-alert alert-${type}`}>
+                        {title}
+                    </div>
+                    <div className="message-alert">
+                        {message}
+                    </div>
+                    <div className="footer-alert">
+                        {action}
+                    </div>
+                </div>
+            </>  
+        )
+    }
+}
+
+export default Alert;
