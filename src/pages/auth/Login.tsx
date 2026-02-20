e4ae770371f507553049a39114022f43565d5540
HOANG\hoang <hoangtran234176@gmail.com>
2026-02-20 15:17:56 +0700

    feat(UI): create login form layout and styling

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,9 +1,171 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import Button from "../../component/ui/Button";
+import Input from "../../component/ui/Input";
+import "./Login.css";
+import logoGoogle from "../../assets/google-logo.png";
+
+
+
 function Login(){
+    const navigate = useNavigate();
+    const [isShowPassword, setIsShowPassword] = useState(false);
+    const [stateTypePassword, setTypePassword] = useState("password");
+    const [btnTextPassword, setBtnTextPassword] = useState("Hiện");
+    
+    const [userName, setUserName] = useState("");
+    const [password, setPassword] = useState("");
+
+    const [isErrorUserName, setIsErrorUserName] = useState(false);
+    const [isErrorPassword, setIsErrorPassword] = useState(false);
+
+    const [validationError, setValidationError] = useState(false);
+    const [authError, setAuthError] = useState(false);
+
+    const handClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+        if (!isShowPassword) {
+            setIsShowPassword(true);
+            setTypePassword("text");
+            setBtnTextPassword("Ẩn");
+        } else {
+            setIsShowPassword(false);
+            setTypePassword("password");
+            setBtnTextPassword("Hiện");
+        }
+    }
+
+    const handSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        const isUserEmpty = userName.trim() === "";
+        const isPassEmpty = password.trim() === "";
+
+        if (isUserEmpty || isPassEmpty) {
+            if (isUserEmpty) {
+                setIsErrorUserName(true);
+                setValidationError(true);
+                setAuthError(false);
+            } 
+            if (isPassEmpty) {
+                setIsErrorPassword(true);
+                setValidationError(true);
+                setAuthError(false);
+            }
+
+            setTimeout(() => {
+                setIsErrorUserName(false);
+                setIsErrorPassword(false);
+            }, 200);
+        } else {
+            if (userName === "admin" && password === "admin"){
+                navigate("/");
+            } else {
+                setAuthError(true);
+                setPassword("");
+                setValidationError(false);
+
+                setIsErrorPassword(true);
+                setIsErrorUserName(true);
+
+                setTimeout(() => {
+                    setIsErrorUserName(false);
+                    setIsErrorPassword(false);
+                }, 200);
+            }
+        }
+
+    }
+
+
     return (
-        <div>
-            <h1>Login</h1>
+        <div className="login-container">
+            <div className="form-login-group">
+                <form className="form-login" onSubmit={handSubmit}>
+                    <h1>ĐĂNG NHẬP</h1>
+                    {MessageError(validationError, authError)}
+                    <Input
+                        type="text"
+                        label="Tên đăng nhập"
+                        onChange={event => setUserName(event.target.value)}
+                        value={userName}
+                        className={isErrorUserName ? "input-error" : ""}
+                    />
+                    <div className="password-group">
+                        <Input
+                            type={stateTypePassword}
+                            label="Mật khẩu"
+                            onChange={event => setPassword(event.target.value)}
+                            value={password}
+                            className={isErrorPassword ? "input-error" : ""}
+                        /> 
+
+                        <Button
+                            type="button"
+                            variant="secondary"
+                            width="72px"
+                            height="48px"
+                            className="btn-show-password"
+                            onClick={handClick}
+                        >
+                            {btnTextPassword}
+                        </Button>
+                    </div>
+                    
+                    <Button 
+                        type="submit" 
+                        variant="primary" 
+                        width="90%" 
+                        height="48px"
+                        className="btn-login"
+                    >
+                        Đăng nhập
+                    </Button>
+                    <div className="forgot-password-group">
+                        <p>Quên mật khẩu?</p>
+                    </div>
+                </form>
+
+                <div className="or-group">
+                    <hr />
+                    <p>Hoặc</p>
+                </div>
+
+                <div className="login-other">
+                    <Button
+                        type="button"
+                        variant="secondary"
+                        width="100%"
+                        height="48px"
+                        className="btn-login-google"
+                    >
+                        <img src={logoGoogle} alt="Logo Google" width={"24px"}/>
+                        <p>Đăng nhập bằng Google</p>    
+                    </Button>
+
+                    <span>Nếu chưa có tài khoản? <Link to="/register">Đăng ký</Link></span>
+                </div>
+            </div>
         </div>
     )
 }
 
 export default Login;
+
+function MessageError(validationError: boolean, authError: boolean) {
+    if (validationError === false && authError === false){
+        return null; 
+    } else if (validationError === true) {
+        return (
+            <div className="message-error">
+                <h4>Vui lòng nhập đầy đủ thông tin</h4>
+            </div>
+        )
+    }  else if (authError === true) {
+        return (
+            <div className="message-error">
+                <h4>Tên đăng nhập hoặc mật khẩu không đúng</h4>
+            </div>
+        )
+    }
+}
